f0d186f682ad56cce2f44c3befaf4e36a83aae4c
Pradeep Edirisinghe <pradeepe@netzero.net>
2020-06-20 21:56:20 -0700

    HomePage Title Test

--- src/test/java/Test/HomePage.java
+++ src/test/java/Test/HomePage.java
@@ -2,16 +2,20 @@ package Test;
 
 import java.io.IOException;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import resources.base;
 
 public class HomePage extends base {
-	
+
 	@Test
-	
+
 	public void basePageNavigation() throws IOException {
-		
-		driver=getBrowser();
+
+		driver = getBrowser();
+		String actual = "QA Click Academy | Selenium,Jmeter,SoapUI,Appium,Database testing,QA Training Academy";
 		driver.get("http://qaclickacademy.com");
+		String expected = driver.getTitle();
+		Assert.assertEquals(actual,expected);
 	}
 }
