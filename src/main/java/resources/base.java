37b415ef3a7e5c6bb23496290d741f6973923ab2
Pradeep Edirisinghe <pradeepe@netzero.net>
2020-06-21 00:26:08 -0700

    Added Screenshot using listeners but for some reason I'm not seen the screenshot folder.

--- src/main/java/resources/base.java
+++ src/main/java/resources/base.java
@@ -1,31 +1,34 @@
 package resources;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class base {
-	
+
 	public WebDriver driver;
-	
+
 	public WebDriver getBrowser() throws IOException {
-		
 
 		Properties prop = new Properties();
-		
-		FileInputStream fis =new FileInputStream ("C:\\ide-ws\\esp\\src\\main\\java\\resources\\data.properties");
-		
+
+		FileInputStream fis = new FileInputStream("C:\\ide-ws\\esp\\src\\main\\java\\resources\\data.properties");
+
 		prop.load(fis);
-		
+
 		String browserName = prop.getProperty("browser");
-		
+
 		switch (browserName) {
 		case "chrome":
 			WebDriverManager.chromedriver().setup();
@@ -34,11 +37,21 @@ public class base {
 		default:
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
-			break;	
+			break;
 		}
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
-		
+
 	}
 
+	public void getScreenShotPath(String testCaseName, WebDriver driver) throws IOException {
+		System.out.println("getScreenShotPath "+ testCaseName + "  " + driver);
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		System.out.println(source);
+		String destFile = System.getProperty("C:\\ide-ws\\esp\\reports\\" + testCaseName + ".png");
+		FileUtils.copyFile(source, new File(destFile));
+		System.out.println(destFile);
+		System.out.println("getScreenShotPath "+ testCaseName + "  " + driver);
+	}
 }
---
+++ src/test/java/Test/Listeners.java
@@ -0,0 +1,71 @@
+package Test;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import resources.base;
+
+public class Listeners extends base implements ITestListener{
+
+	@Override
+	public void onTestStart(ITestResult result) {
+
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		
+		WebDriver driver = null;
+
+		String testMethodName = result.getMethod().getMethodName();
+		
+		try {
+			driver = (WebDriver)result.getTestClass().getRealClass().getDeclaredField("driver").get(result.getInstance());
+		} catch (Exception e) {
+			
+		}
+		try {
+			System.out.println("getScreenShotPath "+ testMethodName + "  " + driver);
+			getScreenShotPath(testMethodName,driver);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+
+	}
+
+	@Override
+	public void onTestFailedWithTimeout(ITestResult result) {
+
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onFinish(context);
+	}
+
+}
