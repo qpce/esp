b59d0e0858dd4ff64d6b3490daf581a6ca603404
Pradeep Edirisinghe <pradeepe@netzero.net>
2020-06-21 11:43:18 -0700

    Extend Report Added to the project with Single Execution only

--- src/test/java/Test/Listeners.java
+++ src/test/java/Test/Listeners.java
@@ -7,22 +7,32 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+import resources.ExtentReporterNG;
 import resources.base;
 
 public class Listeners extends base implements ITestListener{
-
+	
+	ExtentTest test;
+	ExtentReports extent = ExtentReporterNG.getReportObject();
+	
 	@Override
 	public void onTestStart(ITestResult result) {
-
+		test = extent.createTest(result.getMethod().getMethodName());
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
+		test.log(Status.PASS, "Test Passed");
 
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
+		test.fail(result.getThrowable());
 		
 		WebDriver driver = null;
 
@@ -65,7 +75,8 @@ public class Listeners extends base implements ITestListener{
 	@Override
 	public void onFinish(ITestContext context) {
 		// TODO Auto-generated method stub
-		ITestListener.super.onFinish(context);
+		extent.flush();
+		//ITestListener.super.onFinish(context);
 	}
 
 }
